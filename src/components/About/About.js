a56fab6f127ff7c704cd5a3b3a87fe8a78d730f8
Jordan Tehranchi <jordan.tehranchi@yahoo.com>
2022-04-21 12:08:03 -0700

    Added assets, pics, About, Card, NavBar, Work pages and implemented About page so far

---
+++ src/components/About/About.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Avatar from '@mui/material/Avatar';
+import myPic from "../../assets/pics/profile_pic.jpeg";
+
+const About = () => {
+  return (
+    <>
+        <Grid justifyContent="center" container padding={2} spacing={2}>
+            <Grid item xs={12} md={4}>
+                <Avatar alt="Picture of Me!" src={myPic} sx={{width: 1, height: 1}}></Avatar>
+            </Grid> 
+
+            <Grid item md={1}></Grid>
+
+            <Grid item xs={12} md={3}> 
+            <h1>Jordan Tehranchi</h1>
+            <p>lorem asdsa d asd asd asd asd a sd asd a sd asd as d asd as d</p>
+            </Grid>
+        </Grid>
+    </>
+  )
+}
+
+export default About
